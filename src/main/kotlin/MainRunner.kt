2de264f61969b8a075fc434359c80a14438f123d
Arkadiusz Bulski <arek.bulski@gmail.com>
2023-12-06 06:24:17 +0100

    Updated MainRunner ProcessingOperation: WIP

--- src/main/kotlin/MainRunner.kt
+++ src/main/kotlin/MainRunner.kt
@@ -1,6 +1,19 @@
+import com.github.ajalt.mordant.terminal.Terminal
 
 class MainRunner (
     val subprocessor: Processor
 ) {
+
+    fun main () {
+        var operation = ProcessingOperation().apply {
+            profile = Profile().apply {
+                operation = "backup"
+                algorithm = "full"
+                sourcePath = "temporary/source"
+                destinationPath = "temporary/destination1"
+            }
+            terminal = Terminal()
+        }
+    }
 
 }
--- src/main/kotlin/ProcessingOperation.kt
+++ src/main/kotlin/ProcessingOperation.kt
@@ -1,6 +1,8 @@
+import com.github.ajalt.mordant.terminal.Terminal
 
 class ProcessingOperation {
 
     var profile: Profile? = null
+    var terminal: Terminal? = null
 
 }
