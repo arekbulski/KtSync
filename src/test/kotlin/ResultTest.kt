142d357213a6c0ce102af24e49957377825a7a9c
Arkadiusz Bulski <arek.bulski@gmail.com>
2023-12-06 13:06:20 +0100

    ResultTest: small update

--- src/test/kotlin/ResultTest.kt
+++ src/test/kotlin/ResultTest.kt
@@ -7,10 +7,10 @@ class ResultTest {
     fun testToString() {
         val r1 = Result(ResultStatus.Success)
         assert(r1.toString() == "Success")
-        val r2 = Result(ResultStatus.Failure, "unknown issue")
-        assert(r2.toString() == "Failure (unknown issue)")
-        val r3 = Result(ResultStatus.Partial, "I/O", IOException())
-        assert(r3.toString() == "Partial (I/O) caused by java.io.IOException")
+        val r2 = Result(ResultStatus.Partial, "unknown issue")
+        assert(r2.toString() == "Partial (unknown issue)")
+        val r3 = Result(ResultStatus.Failure, "I/O", IOException())
+        assert(r3.toString() == "Failure (I/O) caused by java.io.IOException")
     }
 
 }
