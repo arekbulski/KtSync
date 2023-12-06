c0389e28fc0e528c471830bb298873d6f89daeba
Arkadiusz Bulski <arek.bulski@gmail.com>
2023-12-06 08:53:32 +0100

    Debug class: backupProcess updated.

--- src/main/kotlin/Debug.kt
+++ src/main/kotlin/Debug.kt
@@ -4,8 +4,11 @@ class Debug (
 ) : Processor () {
 
     override fun backupProcess(operation: ProcessingOperation): Result {
-        // TODO: Debug methods are lacking in printing details.
-        return subprocessor.backupProcess(operation)
+        operation.terminal?.println("(debug) into backupProcess (operation has no details yet)")
+        val subresult = subprocessor.backupProcess(operation)
+        if (subprocessor !is DoNothing)
+            operation.terminal?.println("(debug) out of backupProcess (operation has no details yet)")
+        return subresult
     }
 
     override fun backupFolder(folder: ProcessingFile): Result {
