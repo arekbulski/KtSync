d42675fccd4580547893d989a0ccc3b99a75ecf8
Arkadiusz Bulski <arek.bulski@gmail.com>
2023-12-07 00:53:45 +0100

    ExceptionsTest updated: removed stale code.

--- src/test/kotlin/ExceptionsTest.kt
+++ src/test/kotlin/ExceptionsTest.kt
@@ -5,52 +5,29 @@ import kotlin.time.toDuration
 
 class ExceptionsTest {
 
-    private fun callForException (level:Int) {
-        if (level > 1)
-            return callForException(level-1)
-        else
-            throw NotImplementedError()
+    private fun callForException() {
+        throw NotImplementedError()
     }
 
-//    class CheapException : Throwable(null, null, false, false)
-//
-//    private fun callForCheapException (level:Int) {
-//        if (level > 1)
-//            return callForCheapException(level-1)
-//        else
-//            throw CheapException()
-//    }
-
-    private fun callForResult (level:Int): Boolean {
-        if (level > 1)
-            return callForResult(level-1)
-        else
-            return true
+    private fun callForResult(): Boolean {
+        return true
     }
 
     @Test
     fun timeitExceptions() {
         val heavyExceptionTook = measureTime {
             try {
-                callForException(10)
+                callForException()
             }
             catch (e: NotImplementedError) {
             }
         }
-//        val cheapExceptionTook = measureTime {
-//            try {
-//                callForCheapException(10)
-//            }
-//            catch (e: CheapException) {
-//            }
-//        }
         val resultTook = measureTime {
-            val result = callForResult(10)
+            val result = callForResult()
             if (! result)
                 throw NotImplementedError()
         }
         assert(resultTook < heavyExceptionTook/10.0)
-//        assert(heavyExceptionTook < cheapExceptionTook)
         assert(heavyExceptionTook < 0.5.toDuration(DurationUnit.MILLISECONDS))
     }
 
